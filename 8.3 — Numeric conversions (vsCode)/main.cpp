cd4322c8596abc8e349fc36b9b38934796f0ad80
Oureyelet <oureyelet@gmail.com>
2021-07-02 21:43:44 +0100

    2 July 2021, 21:43

--- 8.3 — Numeric conversions (vsCode)/main.cpp
+++ 8.3 — Numeric conversions (vsCode)/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip> // for set::precisions()
 
 void someFcn(int i)
 {
@@ -61,12 +62,60 @@ int main()
     conversion explicit by using static_cast. For example:
     */
 
-    double d{ 5.0 };
+    double d2{ 5.0 };
 
     someFcn(d);// bad: will generate compiler warning about narrowing conversion
 
-    someFcn( static_cast<int>(d) );// good: we're explicitly telling the compiler this narrowing conversion is expected, no warning generated
+    someFcn( static_cast<int>(d) );// good: we're explicitly telling the compiler this narrowing 
+                                   //conversion is expected, no warning generated.
 
+    std::cout << std::endl;
+    /////////////////////////////////////////////////////////////////////////////
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    std::cout << "Brace initialization disallows narrowing conversions" << '\n';
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    /////////////////////////////////////////////////////////////////////////////
+    /*
+    Narrowing conversions are strictly disallowed when using brace initialization 
+    (which is one of the primary reasons this initialization form is preferred):
+    */
+
+    //    int x { 3.5 }; // won't compile
+          int x2 = 3.5;
+    
+    std::cout << std::endl;
+    /////////////////////////////////////////////////////////////////////////////
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    std::cout << "More on numeric conversions" << '\n';
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    /////////////////////////////////////////////////////////////////////////////
+    /*
+    The specific rules for numeric conversions are complicated and numerous, so here are the most 
+    important things to remember.
+
+    In all cases, converting a value into a type whose range doesn’t support that value will lead to 
+    results that are probably unexpected. For example:
+    */
+
+    int i22 { 30000 };
+    char c = i22; // chars have range -128 to 127
+
+    std::cout << static_cast<int>(c) << '\n';
+
+    /*
+    In this example, we’ve assigned a large integer to a variable with type char 
+    (that has range -128 to 127). This causes the char to overflow, and produces an unexpected result:
+    48
+    */
+
+    /*
+    In the case of floating point values, some rounding may occur due to a loss of precision in 
+    the smaller type. For example:
+    */
+
+    float f1 = 0.123456789; // double value 0.123456789 has 9 significant digits, but float can only support about 7
+    std::cout << "Before set precision: "<< f1 << '\n';
+    std::cout << "After set precision: " << std::setprecision(9) << f1 << '\n';
     
 
 
