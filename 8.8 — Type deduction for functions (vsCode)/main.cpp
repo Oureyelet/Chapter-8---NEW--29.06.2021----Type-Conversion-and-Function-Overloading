62a2e2dd1b4cefcc5eb53e753b1838393af17109
Oureyelet <oureyelet@gmail.com>
2021-07-08 15:07:43 +0100

    Eighth of July the year of our Lord 2021 (15:07)

--- 8.8 — Type deduction for functions (vsCode)/main.cpp
+++ 8.8 — Type deduction for functions (vsCode)/main.cpp
@@ -1,13 +1,42 @@
 #include <iostream>
 
+int add(int x, int y)
+{
+    return (x + y);
+}
+
+auto add2(int x, int y) -> int
+{
+    return (x + y);
+}
+
 int main()
 {
     std::cout << std::endl;
     /////////////////////////////////////////////////////////////////////////////
     std::cout << "/////////////////////////////////////////////////////" << '\n';
-    std::cout << "Introduction to type conversion" << '\n';
+    std::cout << "Trailing return type syntax" << '\n';
     std::cout << "/////////////////////////////////////////////////////" << '\n';
     /////////////////////////////////////////////////////////////////////////////
+    
+    //Consider the following function:
+    add(5, 2);
+
+    
+    //Using the trailing return syntax, this could be equivalently written as:
+    add2(2, 5);
+
+    /*
+    For now, we recommend the continued use of the traditional function return syntax 
+    except in situations that require the trailing return syntax.
+    */
+
+    std::cout << std::endl;
+    /////////////////////////////////////////////////////////////////////////////////
+    std::cout << "/////////////////////////////////////////////////////////" << '\n';
+    std::cout << "Type deduction can’t be used for function parameter types" << '\n';
+    std::cout << "/////////////////////////////////////////////////////////" << '\n';
+    /////////////////////////////////////////////////////////////////////////////////
     
 
 
