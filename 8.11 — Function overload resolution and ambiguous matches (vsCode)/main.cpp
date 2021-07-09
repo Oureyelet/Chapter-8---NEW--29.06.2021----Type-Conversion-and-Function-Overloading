a98a20fc8cf060de3f2f8a6fca1b034d59168931
Oureyelet <oureyelet@gmail.com>
2021-07-09 12:43:13 +0100

    Ninth of July the year of our Lord 2021 (12:43)

---
+++ 8.11 — Function overload resolution and ambiguous matches (vsCode)/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+void print(int x)
+{
+    std::cout << x << '\n';
+}
+
+void print(double d)
+{
+    std::cout << d << '\n';
+}
+
+int main()
+{
+    std::cout << std::endl;
+    /////////////////////////////////////////////////////////////////////////////
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    std::cout << "Function overload resolution and ambiguous matches" << '\n';
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    /////////////////////////////////////////////////////////////////////////////
+    /*
+    In simple cases where the type of the function arguments and type of the function parameters 
+    match exactly, this is (usually) straightforward:
+    */
+    print(5);// 5 is an int, so this matches print(int)
+    print(5.5);// 5.5 is a double, so this matches print(double)
+    
+    /*
+    But what happens in cases where the argument types in the function call don’t exactly match the 
+    parameter types in any of the overloaded functions? For example:
+    */
+    //print('a');// char does not match int or double
+    //print(5l);// long does not match int or double
+
+    /*
+    In this lesson, we’ll explore how the compiler matches a given function call to a 
+    specific overloaded function.
+    */
+
+    std::cout << std::endl;
+    /////////////////////////////////////////////////////////////////////////////
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    std::cout << "Resolving overloaded function calls" << '\n';
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    /////////////////////////////////////////////////////////////////////////////
+
+    std::cout << std::endl;
+    /////////////////////////////////////////////////////////////////////////////
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    std::cout << "The argument matching sequence" << '\n';
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    /////////////////////////////////////////////////////////////////////////////
+
+    std::cout << std::endl;
+    /////////////////////////////////////////////////////////////////////////////
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    std::cout << "Ambiguous matches" << '\n';
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    /////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+    return 0;
+}
